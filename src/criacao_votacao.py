c698a4a8ecfafe8d602396eff6d40baab0f3e51f
DEV-T-2025 <moraisgabriel867@gmail.com>
2025-06-11 14:05:14 -0300

    alguns bugs arrumados

--- src/criacao_votacao.py
+++ src/criacao_votacao.py
@@ -101,7 +101,7 @@ def main(page: ft.Page):
         )
     
     titulo = ft.Text("Votações", size=45, weight=ft.FontWeight.BOLD, color="#ffffff")
-    sub_titulo = ft.Text("Defina os detalhes da votação que deseja criar.", size=16)
+    sub_titulo = ft.Text("Defina os detalhes da votação que deseja criar.", size=16, color="#ffffff")
     
     container_titulo = ft.Container(
         content=ft.Column([titulo, sub_titulo], spacing=10, alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER),
@@ -111,19 +111,19 @@ def main(page: ft.Page):
 
     # detalhes_votacao = ft.Text("Detalhes da Votação")
 
-    nome_votacao = ft.Text("Nome da Votação", size=18)
+    nome_votacao = ft.Text("Nome da Votação", size=18, color="#ffffff")
     nome_votacao_label = ft.TextField(hint_text="Insira o nome da votação", bgcolor="#ffffff", color="#000000", border_radius=10, border_color="#352A2A")
     nome_votacao_container = ft.Container(content=ft.Column([nome_votacao, nome_votacao_label], spacing=1))    
 
-    descricao = ft.Text("Descrição", size=18)
+    descricao = ft.Text("Descrição", size=18, color="#ffffff")
     descricao_label = ft.TextField(hint_text="Insira uma breve descrição", bgcolor="#ffffff", color="#000000", border_radius=10, border_color="#352A2A")
     descricao_container = ft.Container(content=ft.Column([descricao, descricao_label], spacing=1))
 
-    categoria = ft.Text("Categoria", size=18)
+    categoria = ft.Text("Categoria", size=18, color="#ffffff")
     categoria_label = ft.TextField(hint_text="Insira uma categoria para a votação", bgcolor="#ffffff", color="#000000", border_radius=10, border_color="#352A2A")
     categoria_container = ft.Container(content=ft.Column([categoria, categoria_label], spacing=1))
 
-    candidaturas = ft.Text("Permitir Candidaturas?", size=18)
+    candidaturas = ft.Text("Permitir Candidaturas?", size=18, color="#ffffff")
     candidaturas_sim_btn = ft.ElevatedButton(text="Sim", 
                                             color="#ffffff", 
                                             expand=True,
@@ -138,11 +138,11 @@ def main(page: ft.Page):
                                             height=45,
                                             on_click=desativar)
     
-    descricao_candidatura = ft.Text("Escolha se os participantes podem se candidatar.", color="#979797")
+    descricao_candidatura = ft.Text("Escolha se os participantes podem se candidatar.", color="#98989898")
     candidaturas_btn_container = ft.Container(content=ft.Row([candidaturas_sim_btn, candidaturas_nao_btn]))
     candidaturas_container = ft.Container(content=ft.Column([candidaturas, candidaturas_btn_container, descricao_candidatura], spacing=1)) 
 
-    periodo_inicio = ft.Text("Data de Início", size=18, opacity=0.6)
+    periodo_inicio = ft.Text("Data de Início", size=18, opacity=0.6, color="#ffffff")
     periodo_inicio_label = ft.TextField(disabled=True, bgcolor="#FFFFFF", opacity=0.6, color="#000000", border_radius=10, border_color="#352A2A")
     periodo_inicio_container = ft.Container(content=ft.Column([periodo_inicio, periodo_inicio_label], spacing=1))
 
@@ -150,13 +150,13 @@ def main(page: ft.Page):
     periodo_termino_label = ft.TextField(disabled=True, bgcolor="#ffffff", opacity=0.6, color="#000000", border_radius=10, border_color="#352A2A")
     periodo_termino_container = ft.Container(content=ft.Column([periodo_termino, periodo_termino_label], spacing=1))
 
-    candidatos = ft.Text("Registrar opção/candidato", size=18)
+    candidatos = ft.Text("Registrar opção/candidato", size=18, color="#FFFFFF")
     candidatos_label = ft.TextField(hint_text="Insira um(a) opcao/candidato", bgcolor="#ffffff", color="#000000", border_radius=10, border_color="#352A2A")
     candidatos_container = ft.Container(content=ft.Column([candidatos, candidatos_label], spacing=1))
 
     candidatos_label.on_change = ativar_detalhes
 
-    detalhes = ft.Text("Detalhes", size=18, opacity=0.6)
+    detalhes = ft.Text("Detalhes", size=18, opacity=0.6, color="#ffffff")
     detalhes_label = ft.TextField(hint_text="Insira informações da(o) opcao/candidato", disabled=True, opacity=0.6, bgcolor="#ffffff", color="#000000", border_radius=10, border_color="#352A2A")
     detalhes_container = ft.Container(content=ft.Column([detalhes, detalhes_label], spacing=1))
 
@@ -168,19 +168,20 @@ def main(page: ft.Page):
     
     adicionar_btn_container = ft.Container(content=ft.Row([adicionar_btn]))
     
-    opcoes_disponiveis_titulo = ft.Text("Opções disponíveis", size=32)
+    opcoes_disponiveis_titulo = ft.Text("Opções disponíveis", size=32, color="#ffffff")
     opcoes_disponiveis_titulo_container = ft.Container(content=ft.Row([opcoes_disponiveis_titulo], alignment=ft.MainAxisAlignment.START))
 
     voltar_btn = ft.ElevatedButton(
         text="Voltar",
-        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10), color="#ffffff", side=ft.BorderSide(width=2, color="#ffffff")),
+        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10), color="#ffffff", side=ft.BorderSide(width=2, color="#ffffff"), text_style=ft.TextStyle(weight=ft.FontWeight.BOLD)),
         expand=True,
-        height=45
+        height=45,
+        bgcolor="#303030"
         )
     
     criar_btn = ft.ElevatedButton(
         text="Criar Votação",
-        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10),bgcolor="#ffffff", color="#000000"),
+        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10),bgcolor="#ffffff", color="#000000", text_style=ft.TextStyle(weight=ft.FontWeight.BOLD)),
         expand=True,
         height=45
     )
