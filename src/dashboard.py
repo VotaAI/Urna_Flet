fb7ce19ea5b2261a5ca9651211434a3868d66bc4
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-06-13 14:49:01 -0300

    Conexoes API

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,4 +1,5 @@
 import flet as ft
+from requests import get
 
 def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -6,46 +7,64 @@ def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.DARK
     page.scroll = ft.ScrollMode.AUTO
 
-    status = ft.Container(
-        content=ft.Text("Status", color=ft.Colors.WHITE),
-        width=200,
-        height=40,
-        bgcolor=ft.Colors.BLACK,
-        alignment=ft.alignment.center,
-        border_radius=10,
-        ink=True,
-        on_click=lambda e: print("Clickable transparent with Ink clicked!"),
-    )
-    excluir = ft.Container(
-        content=ft.Text("Excluir", color=ft.Colors.WHITE),
-        width=200,
-        height=40,
-        bgcolor=ft.Colors.BLACK,
-        alignment=ft.alignment.center,
-        border_radius=10,
-        ink=True,
-        on_click=lambda e: print("Clickable transparent with Ink clicked!"),
-    )
+    votacoes_abertas = get("https://backend-api-urna.onrender.com/votacoes/open?limit=3&offset=0").json()
+    votacoes_fechadas = get("https://backend-api-urna.onrender.com/votacoes/closed?limit=3&offset=0").json()
+    candidatos_pendentes = get("https://backend-api-urna.onrender.com/candidaturas/pendentes?limit=3&offset=0").json()
 
-    title = ft.Text("Nome da votação")
-    periodo = ft.Text("Período: 30/05/2025 até 06/06/2025.")
-    descricao = ft.Text("Descrição: Votação para eleição do novo Representante de Classe.")
+    votacoes_abertas_exibicao = []
+    votacoes_fechadas_exibicao = []
+    candidatos_pendentes_lista = []
 
-    votacao_detalhes = ft.Column(
-        controls=[title,periodo,descricao]
-    )
 
-    votacao = ft.Container(
-        content=votacao_detalhes
-    )
-    
-    row = ft.Row(
-        width=800,
-        controls=[votacao,status,excluir]
-    )
+    def criar_exibicao_votacao(id_votacao, titulo, descricao, data_inicio, data_fim, status, list):
+        btn_status = ft.Container(
+            content=ft.Text("Status", color=ft.Colors.WHITE),
+            width=200,
+            height=40,
+            bgcolor=ft.Colors.BLACK,
+            alignment=ft.alignment.center,
+            border_radius=10,
+            ink=True,
+            on_click=lambda e: print("Clickable transparent with Ink clicked!"),
+        )
+        excluir = ft.Container(
+            content=ft.Text("Excluir", color=ft.Colors.WHITE),
+            width=200,
+            height=40,
+            bgcolor=ft.Colors.BLACK,
+            alignment=ft.alignment.center,
+            border_radius=10,
+            ink=True,
+            on_click=lambda e: print("Clickable transparent with Ink clicked!"),
+        )
 
-    pendentes = ft.Text("Candidatos pendentes", size=34)
-    pendentes_detalhes = ft.Text("Aprovar ou rejeitar candidatos")
+        title = ft.Text(titulo)
+        periodo = ft.Text(f"Período: {data_inicio} até {data_fim}.")
+        description = ft.Text(f"Descrição: {descricao}")
+
+        votacao_detalhes = ft.Column(
+            controls=[title,periodo,description]
+        )
+
+        votacao = ft.Container(
+            content=votacao_detalhes
+        )
+
+        row = ft.Container(
+            content=ft.Row(
+                width=800,
+                controls=[votacao,btn_status,excluir],
+            ),
+            margin=20  # ou ft.Margin(top=10, left=10, right=10, bottom=10)
+        )
+
+        list.append(row)
+
+    for votacao in votacoes_abertas:
+        criar_exibicao_votacao(votacao["id_votacao"], votacao["titulo"], votacao["descricao"], votacao["data_inicio"], votacao["data_fim"], votacao["status"], votacoes_abertas_exibicao)
+
+    for votacao in votacoes_fechadas:
+        criar_exibicao_votacao(votacao["id_votacao"], votacao["titulo"], votacao["descricao"], votacao["data_inicio"], votacao["data_fim"], votacao["status"], votacoes_fechadas_exibicao)
 
     aceitar_todos = ft.Container(
         content=ft.Text("Aceitar todos",color=ft.Colors.BLACK),
@@ -70,6 +89,14 @@ def main(page: ft.Page):
         on_click=lambda e: print("Clickable transparent with Ink clicked!"),
     )
 
+
+
+
+
+
+    pendentes = ft.Text("Candidatos pendentes", size=34)
+    pendentes_detalhes = ft.Text("Aprovar ou rejeitar candidatos")
+
     row_candidatos_pendentes = ft.Row(
         controls=[aceitar_todos,rejeitar_todos],
         alignment=ft.alignment.center,
@@ -80,11 +107,8 @@ def main(page: ft.Page):
         alignment=ft.alignment.center,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         width=800,
-        height=200
+        height=500
     )
-
-    nome_candidato = ft.Text("NOME DO CANDIDATO")
-    candidato_votacao = ft.Text("Votação a qual se candidatou")
 
     aprovar_candidato = ft.Container(
         content=ft.Text("Aprovar"),
@@ -100,27 +124,35 @@ def main(page: ft.Page):
         width=70,
     )
 
-    candidato_info = ft.Column(
-        controls=[nome_candidato,candidato_votacao]
-    )
+    def criar_candidato_exibicao(nome, detalhes, votacao):
+        nome_candidato = ft.Text("NOME DO CANDIDATO")
+        candidato_detalhes = ft.Text("Detalhes")
+        candidato_votacao = ft.Text("Votação a qual se candidatou")
+        candidato_info = ft.Column(
+            controls=[nome_candidato,candidato_detalhes, candidato_votacao]
+        )
 
-    controles_candidato = ft.Row(
-        controls=[aprovar_candidato,rejeitar_candidato]
-    )
+        controles_candidato = ft.Row(
+            controls=[aprovar_candidato,rejeitar_candidato]
+        )
 
-    # img_candidato = ft.Image(
-    #     src='frame.svg',
-    #     width=100,
-    #     height=100,
-    #     fit=ft.ImageFit.CONTAIN,
-    # )
+        # img_candidato = ft.Image(
+        #     src='frame.svg',
+        #     width=100,
+        #     height=100,
+        #     fit=ft.ImageFit.CONTAIN,
+        # )
 
-    candidato_template = ft.Row(
-        controls=[candidato_info,controles_candidato],
-        #alignment=ft.alignment.center,
-        width=800,
-        spacing=450
-    )
+        candidato_template = ft.Row(
+            controls=[candidato_info,controles_candidato],
+            #alignment=ft.alignment.center,
+            width=800,
+            spacing=450
+        )
+        candidatos_pendentes_lista.append(candidato_template)
+
+    for candidato in candidatos_pendentes:
+        criar_candidato_exibicao(candidato["id_user"], candidato["detalhes"], candidato["id_votacao"])
 
     page.add(
         ft.Container(
@@ -140,10 +172,7 @@ def main(page: ft.Page):
             margin=10
         ),
 
-        ft.Container(
-            content=row,
-            margin=5
-        ),
+        *votacoes_abertas_exibicao,
 
         ft.Container(
             content=ft.Text("Criar Votação", color=ft.Colors.BLACK),
@@ -164,10 +193,7 @@ def main(page: ft.Page):
             width=800,
         ),
 
-        ft.Container(
-            content=row,
-            margin=20
-        ),
+        *votacoes_fechadas_exibicao,
 
         ft.Container(
             content=col_candidatos_pendentes,
@@ -176,7 +202,7 @@ def main(page: ft.Page):
             height=600
         ),
 
-        candidato_template,
+        *candidatos_pendentes_lista,
     ),
 
 ft.app(main)
